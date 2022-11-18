028ce6cfe7277abc0cf7958f79a6e35e5531ba30
Aman Srivastava <89743268+amansrv@users.noreply.github.com>
2022-11-18 13:06:37 +0530

    Update MaximumSubarray.cpp

--- MaximumSubarray.cpp
+++ MaximumSubarray.cpp
@@ -1,5 +1,6 @@
 // 53. Maximum Subarray @Leetcode
 
+// C++ code: Retrun Maximum Subarray
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
@@ -17,3 +18,26 @@ public:
         return maxi;
     }
 };
+
+// Java code: Retrun first index of Maximum Subarray
+public class maxsubindex {
+    public static void main(String[] args) {
+        int[] arr={-2, 4, -5, 10, 9, -7, 0, 5, 20};
+        System.out.println(indexfind(arr));
+    }
+    public static int indexfind(int[] arr){
+        int res=arr[0];
+        int ans=0;
+        for(int i=0;i<arr.length;i++){
+            int curr=0;
+            for(int j=i;j<arr.length;j++){
+                curr+=arr[i];
+                if(curr>res){
+                    res=curr;
+                    ans=i;
+                }
+            }
+        }
+        return ans;
+    }
+}
